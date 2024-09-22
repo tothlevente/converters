1e275d546b56213ce2885c921b5178a7ef7d02dc
tothlevente <tothlevente139@gmail.com>
2024-09-22 10:24:20 +0200

    Developments

--- src/components/Temperatures/Temperatures.tsx
+++ src/components/Temperatures/Temperatures.tsx
@@ -1,12 +1,32 @@
+import TemperaturesForm from "./TemperaturesForm";
+
+import { grey, red } from "@mui/material/colors";
 import { Box, Typography } from "@mui/material";
-import { grey } from "@mui/material/colors";
 import { TemperaturesLogo } from "../Logo";
 
 export default function Temperatures() {
   return (
-    <Box sx={{ color: grey[800] }}>
-      <TemperaturesLogo width={50} height={50} />
-      <Typography variant="h3">Temperatures</Typography>
+    <Box
+      sx={{
+        color: grey[800],
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        marginTop: "50px",
+        marginBottom: "50px",
+      }}
+    >
+      <TemperaturesLogo width={50} height={50} color={red[500]} />
+      <Typography variant="h4" sx={{ marginBottom: "10px", fontWeight: "bold" }}>
+        Temperatures
+      </Typography>
+      <Typography variant="subtitle1" sx={{ marginBottom: "15px", maxWidth: "500px" }}>
+        To convert different temperatures, please enter the value of the temperature measurement
+        what you want to convert and you will receive the calculated values in the other two
+        places.
+      </Typography>
+      <TemperaturesForm />
     </Box>
   );
 }
